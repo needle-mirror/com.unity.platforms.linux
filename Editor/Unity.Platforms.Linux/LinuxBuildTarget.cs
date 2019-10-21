006f06c7d109ed01cb613acd24e7f89e18298889
Unity Technologies <@unity.com>
2019-10-22 00:00:00 +0200

    com.unity.platforms.linux@0.1.5-preview ## [0.1.5-preview] - 2019-10-22
    ### Changed
    * Updated `com.unity.platforms` to `0.1.5-preview`.
    * Linux build targets `HideInBuildTargetPopup` is now `false`.
    
    ## [0.1.4-preview] - 2019-09-26
    * Bug fixes
    * Add iOS platform support
    * Add desktop platforms package

--- Editor/Unity.Platforms.Linux/LinuxBuildTarget.cs
+++ Editor/Unity.Platforms.Linux/LinuxBuildTarget.cs
@@ -1,29 +1,14 @@
 using System.Diagnostics;
 using System.IO;
+using Debug = UnityEngine.Debug;
 
 namespace Unity.Platforms.Linux
 {
     public abstract class LinuxBuildTarget : BuildTarget
     {
-        public override bool HideInBuildTargetPopup => UnityEngine.Application.platform != UnityEngine.RuntimePlatform.LinuxEditor;
-
         public override string GetUnityPlatformName()
         {
             return nameof(UnityEditor.BuildTarget.StandaloneLinux64);
-        }
-
-        public override bool Run(FileInfo buildTarget)
-        {
-            var startInfo = new ProcessStartInfo();
-            startInfo.Arguments = $"\"{buildTarget.FullName.Trim('\"')}\"";
-            startInfo.FileName = Path.GetFullPath(Path.Combine(UnityEditor.EditorApplication.applicationContentsPath, "MonoBleedingEdge", "bin", "mono"));
-            startInfo.WorkingDirectory = buildTarget.Directory.FullName;
-            if (!startInfo.EnvironmentVariables.ContainsKey("LD_LIBRARY_PATH"))
-				startInfo.EnvironmentVariables.Add("LD_LIBRARY_PATH",".");
-            startInfo.CreateNoWindow = true;
-            startInfo.UseShellExecute = false;
-            var process = Process.Start(startInfo);
-            return process != null;
         }
     }
 
@@ -43,9 +28,57 @@ namespace Unity.Platforms.Linux
             return "linux-dotnet";
         }
         public override string GetExecutableExtension()
-		{
-			return ".exe";
-		}
+        {
+            return ".exe";
+        }
+
+        public override bool Run(FileInfo buildTarget)
+        {
+            var startInfo = new ProcessStartInfo();
+            startInfo.Arguments = $"\"{buildTarget.FullName.Trim('\"')}\"";
+            startInfo.FileName = Path.GetFullPath(Path.Combine(UnityEditor.EditorApplication.applicationContentsPath, "MonoBleedingEdge", "bin", "mono"));
+            startInfo.WorkingDirectory = buildTarget.Directory.FullName;
+            if (!startInfo.EnvironmentVariables.ContainsKey("LD_LIBRARY_PATH"))
+                startInfo.EnvironmentVariables.Add("LD_LIBRARY_PATH",".");
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+
+            var process = new Process();
+            process.StartInfo = startInfo;
+            process.OutputDataReceived += (_, args) => Debug.Log(args.Data);
+            process.ErrorDataReceived += (_, args) => Debug.LogError(args.Data);
+            
+            var success = process.Start();
+            if (!success)
+                return false;
+
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            return true;
+        }
+
+        public override ShellProcessOutput RunTestMode(string exeName, string workingDirPath, int timeout)
+        {
+            var shellArgs = new ShellProcessArgs
+            {
+                Executable = Path.GetFullPath(Path.Combine(UnityEditor.EditorApplication.applicationContentsPath, "MonoBleedingEdge", "bin", "mono")),
+                Arguments = new [] { $"\"{workingDirPath}/{exeName}{GetExecutableExtension()}\"" },
+                WorkingDirectory = new DirectoryInfo(workingDirPath),
+                ThrowOnError = false
+            };
+
+            // samples should be killed on timeout
+            if (timeout > 0)
+            {
+                shellArgs.MaxIdleTimeInMilliseconds = timeout;
+                shellArgs.MaxIdleKillIsAnError = false;
+            }
+
+            return Shell.Run(shellArgs);
+        }
     }
 
     class IL2CPPLinuxBuildTarget : LinuxBuildTarget
@@ -60,8 +93,55 @@ namespace Unity.Platforms.Linux
             return "linux-il2cpp";
         }
         public override string GetExecutableExtension()
-		{
-			return string.Empty;
-		}
+        {
+            return string.Empty;
+        }
+
+        public override bool Run(FileInfo buildTarget)
+        {
+            var startInfo = new ProcessStartInfo();
+            startInfo.FileName = buildTarget.FullName.Trim('.');
+            startInfo.WorkingDirectory = buildTarget.Directory.FullName;
+            if (!startInfo.EnvironmentVariables.ContainsKey("LD_LIBRARY_PATH"))
+                startInfo.EnvironmentVariables.Add("LD_LIBRARY_PATH",".");
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+
+            var process = new Process();
+            process.StartInfo = startInfo;
+            process.OutputDataReceived += (_, args) => Debug.Log(args.Data);
+            process.ErrorDataReceived += (_, args) => Debug.LogError(args.Data);
+            
+            var success = process.Start();
+            if (!success)
+                return false;
+
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            return true;
+        }
+
+        public override ShellProcessOutput RunTestMode(string exeName, string workingDirPath, int timeout)
+        {
+            var shellArgs = new ShellProcessArgs
+            {
+                Executable = $"{workingDirPath}/{exeName}{GetExecutableExtension()}",
+                Arguments = new string [] {},
+                WorkingDirectory = new DirectoryInfo(workingDirPath),
+                ThrowOnError = false
+            };
+
+            // samples should be killed on timeout
+            if (timeout > 0)
+            {
+                shellArgs.MaxIdleTimeInMilliseconds = timeout;
+                shellArgs.MaxIdleKillIsAnError = false;
+            }
+
+            return Shell.Run(shellArgs);
+        }
     }
 }
